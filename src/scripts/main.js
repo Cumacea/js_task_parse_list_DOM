658407395815996e63d32cd409c81f10be8d3e1a
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2025-12-18 13:25:26 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write code here
+const list = [...document.querySelectorAll('li')];
+
+function sortList() {
+  const unorderedList = document.querySelector('ul');
+
+  list.sort((a, b) => {
+    const salaryA = +a.dataset.salary.replace(/\D/g, '');
+    const salaryB = +b.dataset.salary.replace(/\D/g, '');
+
+    return salaryB - salaryA;
+  });
+
+  list.forEach((el) => unorderedList.append(el));
+}
+
+function getEmployees() {
+  return list.map((el) => {
+    return {
+      name: el.textContent.trim(),
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    };
+  });
+}
+
+sortList();
+getEmployees();
